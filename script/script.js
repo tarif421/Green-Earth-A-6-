8eb789652823b26eb0c7cb63f918c6b6a1a11b58
tarif421 <tarif305@gmail.com>
2025-09-07 23:25:15 +0600

    dynamic card added

--- script/script.js
+++ script/script.js
@@ -3,15 +3,50 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((title) => displayCategories(title.categories));
 };
+//3
+const loadCard = (id) => {
+  const url = `https://openapi.programming-hero.com/api/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((card) => displayCards(card.plants));
+};
+const displayCards = (cards) => {
+  const cardsContainer = document.getElementById("card-container");
+  cardsContainer.innerHTML = "";
+  for (const card of cards) {
+    const cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
+    <div class="bg-white p-5 h-full flex flex-col justify-between rounded-2xl shadow-md ">
+                        <div class=" rounded-2xl ">
+                            <img class=" rounded-2xl h-40 w-full object-cover" src="${card.image}" alt="">
+                        </div>
+                        <div>
+                            <h2 class="font-semibold text-xl mt-2">${card.name}</h2>
+                            <p class="text-[#1F293780] mt-2 text-[10px]">${card.description}</p>
+                        </div>
+                        <div class="flex justify-between mt-3">
+                            <button class="text-[#15803D] bg-[#DCFCE7] py-1 px-2 rounded-full text-[10px]">Fruit
+                                Tree</button>
+                            <p><i class="fa-solid fa-bangladeshi-taka-sign"></i> <span class="font-bold">${card.price}</span></p>
+                        </div>
+                        <button class="btn btn-success w-full  border-none text-left  text-white rounded-full mt-3">Add
+                            to Cart</button>
+                    </div>
+    `;
+    cardsContainer.append(cardDiv);
+  }
+};
+window.loadCard = loadCard;
+
+// loadCategory();
 const displayCategories = (titles) => {
   const titleContainer = document.getElementById("Tree-options-container");
   titleContainer.innerHTML = "";
 
   for (const title of titles) {
-    console.log(title);
     const btnTitles = document.createElement("div");
-    btnTitles.innerHTML = `
-                       <button
+    btnTitles.innerHTML = `    
+                       <button onclick="loadCard('${title.id}')"
                         class="btn btn-outline btn-success w-full  border-none text-left justify-start text-[#1F2937]">${title.category_name}</button>
     `;
     titleContainer.append(btnTitles);
