680b374ec0726731df28adcbb5dd6e9d56179e1b
tarif421 <tarif305@gmail.com>
2025-09-08 19:41:14 +0600

    modal card added

--- script/script.js
+++ script/script.js
@@ -10,7 +10,48 @@ const removeActive = () => {
   activeBtns.forEach((btn) => btn.classList.remove("active"));
 };
 
-//3
+//modal
+const loadTreeDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayTreeDetails(details.plants);
+};
+const displayTreeDetails = (treeDetails) => {
+  const detailTrees = document.getElementById("modal-container");
+  detailTrees.innerHTML = `
+  <div class="p-4">
+                        <h2 class="font-bold text-xl mb-2">${treeDetails.name}</h2>
+                        <img class="rounded-2xl h-60 w-full object-cover" src="${treeDetails.image}" alt="">
+                        <div class="flex mb-2">
+                            <h4 class="font-medium">Category: </h4>
+                            <p class="text-[#1F293790]"> ${treeDetails.category}</p>
+                        </div>
+                         <div class="flex mb-2">
+                        <h4 class="font-medium" >Price:</h4>
+                        <p class="text-[#1F293790]"><span class="text-[#1F293790]"><i
+                                    class="fa-solid fa-bangladeshi-taka-sign"></i></span> ${treeDetails.price}</p>
+                    </div>
+                        <div class="flex mb-2">
+                            <h4 class="font-medium" >Description: </h4>
+                            <p class="text-[#1F293790] ">${treeDetails.description}</p>
+                        </div>
+                        <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn p-1">Close</button>
+                </form>
+            </div>
+
+                    </div>
+
+  `;
+  // for (const)
+  document.getElementById("my_modal_5").showModal();
+};
+
+window.loadTreeDetails = loadTreeDetails;
+//load card
 const loadCard = (id) => {
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
@@ -35,12 +76,12 @@ const displayCards = (cards) => {
                             <img class=" rounded-2xl h-40 w-full object-cover" src="${card.image}" alt="">
                         </div>
                         <div>
-                            <h2 class="font-semibold text-xl mt-2">${card.name}</h2>
+                            <button onclick="loadTreeDetails('${card.id}')" class="font-semibold text-xl mt-2">${card.name}</button>
                             <p class="text-[#1F293780] mt-2 text-[10px]">${card.description}</p>
                         </div>
                         <div class="flex justify-between mt-3">
-                            <button class="text-[#15803D] bg-[#DCFCE7] py-1 px-2 rounded-full text-[10px]">Fruit
-                                Tree</button>
+                            <button class="text-[#15803D] bg-[#DCFCE7] py-1 px-2 rounded-full text-[10px]"> ${card.category}
+                                </button>
                             <p><i class="fa-solid fa-bangladeshi-taka-sign"></i> <span class="font-bold">${card.price}</span></p>
                         </div>
                         <button   class="btn btn-success w-full  border-none text-left  text-white rounded-full mt-3 cart ">Add
@@ -59,6 +100,7 @@ const displayCards = (cards) => {
       crtDiv.className = "flex justify-between items-center ";
       crtDiv.innerHTML = `
                         <div>
+                              
                             <h3 class="font-medium">${card.name}</h3>
                             <p><i class="fa-solid fa-bangladeshi-taka-sign"></i><span>${card.price}</span> x 1</p>
                         </div>
@@ -80,10 +122,9 @@ const displayCards = (cards) => {
 
 window.loadCard = loadCard;
 
-// loadCategory();
 const displayCategories = (titles) => {
   const titleContainer = document.getElementById("Tree-options-container");
-  titleContainer.innerHTML = "";
+  // titleContainer.innerHTML = "";
 
   for (const title of titles) {
     const btnTitles = document.createElement("div");
